fa47f0874ef8c4518b3ca2295075d803abf7b028
Ahyarnur <ahyarahyar5741@gmail.com>
2025-02-13 16:50:53 +0700

    update

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -2,40 +2,7 @@
 
 @section('content')
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-      <div class="d-flex align-items-center">
-          <img src="logo.png" height="50px" alt="">
-          <div class="mt-3" style="margin-left: 10px">
-              <p class="fs-4" style="margin-bottom: 30px">Desa Ajibarang</p>
-              <p style="margin-top: -37px">Kec.Ajibarang Kab.Banyumas</p>
-          </div>
-      </div>
-    <div class="collapse navbar-collapse" style="margin-left: 400px" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+
 
     <div class="position-relative" style="background-image: url('/ya.png'); background-size: cover; background-position: center; height: 100vh;">
         <div class="position relative d-flex flex-column justify-content-center align-items-center h-100">
--- resources/views/layouts/ya.blade.php
+++ resources/views/layouts/ya.blade.php
@@ -7,7 +7,40 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <div class="d-flex align-items-center">
+                <img src="logo.png" height="50px" alt="">
+                <div class="mt-3" style="margin-left: 10px">
+                    <p class="fs-4" style="margin-bottom: 30px">Desa Ajibarang</p>
+                    <p style="margin-top: -37px">Kec.Ajibarang Kab.Banyumas</p>
+                </div>
+            </div>
+          <div class="collapse navbar-collapse" style="margin-left: 400px" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Features</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Pricing</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown link
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
       <main class="py-4">
         @yield('content')
     </main>
