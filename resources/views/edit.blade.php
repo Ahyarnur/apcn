0816d1fcbaeb0c9795d40b39d82940eaf90c048b
Ahyarnur <ahyarahyar5741@gmail.com>
2025-02-14 18:12:24 +0700

    cek

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body class="justify-content-center">
-    <h1 class="text-center mt-5">Tambah Berita</h1>
+    <h1 class="text-center mt-5">Edit Berita</h1>
     <div style="padding: 50px">
 
         <form enctype="multipart/form-data" method="POST" action="{{route('update', ['id'=>$edit])}}">
